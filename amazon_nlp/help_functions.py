354267def53430b1aaf457719565b634bcb3f4d5
JD Mayer <140545470+UsuallyJD@users.noreply.github.com>
2023-11-02 20:29:48 -0700

    Update help_functions.py
    For class restructuring
    Added generate word cloud function

--- amazon_nlp/help_functions.py
+++ amazon_nlp/help_functions.py
@@ -1,20 +1,22 @@
 '''
-file docstring
+helper functions for review_analysis core file
 '''
 import re
 import numpy as np
 import matplotlib.pyplot as plt
 
-import gensim
-from gensim.models import CoherenceModel
-
-from multiprocessing import freeze_support
-freeze_support()
+from wordcloud import WordCloud
 
 def clean_data(text):
-    """
-    Cleans data to remove unwanted characters and punctuation.
-    """
+    '''
+    cleans data to remove unwanted characters and punctuation.
+
+    parameters:
+        text: string
+
+    returns:
+        text: modified string
+    '''
     text.replace('\\n', ' ')
     text = re.sub(r'[ ]{2, }', ' ', text)
     text = re.sub(r'[^a-zA-Z ]', '', text)
@@ -23,101 +25,30 @@ def clean_data(text):
     return text
 
 
-def compute_coherence_values(dictionary, corpus, id2word, texts, limit, start=2, step=3):
-    """
-    Compute c_v coherence for various number of topics
+def get_doc_topic(lda_model, corpus):
+    '''
+    passes bag-of-words vector into trained LDA model to get topic id of document
 
-    Parameters:
-    ----------
-    dictionary : Gensim dictionary
-    corpus : Gensim corpus
-    texts : List of input texts
-    limit : Max num of topics
+    parameters:
+    lda_model: Gensim object (must be trained model)
+    corpus: matrix of tuples (ie list of lists)
 
-    Returns:
-    -------
-    model_list : List of LDA topic models
-    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
-    """
-    coherence_values = []
-    model_list = []
-    for num_topics in range(start, limit, step):
-        model = gensim.models.ldamulticore.LdaMulticore(corpus=corpus,
-                                                        id2word=id2word,
-                                                        num_topics=num_topics,
-                                                        chunksize=100,
-                                                        passes=10,
-                                                        random_state=34,
-                                                        per_word_topics=True,
-                                                        workers=4)
-        model_list.append(model)
-        coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
-        coherence_values.append(coherencemodel.get_coherence())
+    returns:
+        topic_id_list: list
+    '''
 
-    return model_list, coherence_values
-
-def plot_coherence_v_no_topics(x, coherence_values):
-    plt.figure(figsize=(20,5))
-    plt.grid()
-    plt.title("Coherence Score vs. Number of Topics")
-    plt.xticks(x)
-    plt.plot(x, coherence_values, "-o")
-
-    plt.xlabel("Num Topics")
-    plt.ylabel("Coherence score")
-
-    plt.show();
-
-def get_topic_id_lookup_dict(vis, vis_topic_name_dict):
-    """
-    Both the starting index and the ordering of topic ids bewteen the trained LDA model
-    and the viz tool are different. So we need to create a look up dictionary that maps
-    the correct association between topic ids from both sources.
-    """
-    # value is order of topic ids accoridng to pyLDAvis tool
-    # key is order of topic ids according to lda model
-    model_vis_tool_topic_id_lookup = vis.topic_coordinates.topics.to_dict()
-
-    # invert dictionary so that
-    # key is order of topic ids accoridng to pyLDAvis tool
-    # value is order of topic ids according to lda model
-    topic_id_lookup =  {v:k for k, v in model_vis_tool_topic_id_lookup.items()}
-
-    return {v:vis_topic_name_dict[k]  for k, v in topic_id_lookup.items()}
-
-def get_topic_ids_for_docs(lda_model, corpus):
-
-    """
-    Passes a Bag-of-Words vector into a trained LDA model in order to get the topic id of that document.
-
-    Parameters
-    ----------
-    lda_model: Gensim object
-        Must be a trained model
-
-    corpus: nested lists of tuples,
-        i.e. [[(),(), ..., ()], [(),(), ..., ()], ..., [(),(), ..., ()]]
-
-    Returns
-    -------
-    topic_id_list: list
-        Contains topic ids for all document vectors in corpus
-    """
-
-    # store topic ids for each document
+    # create list for most likely topic ids for each document
     doc_topic_ids = []
 
-    # iterature through the bow vectors for each doc
+    # iterature through the bag-of-words vectors for each doc
     for doc_bow in corpus:
 
-        # store the topic ids for the doc
+        # create lists for topic ids and probabilities
         topic_ids = []
-        # store the topic probabilities for the doc
         topic_probs = []
 
-        # list of tuples
         # each tuple has a topic id and the prob that the doc belongs to that topic
-        topic_id_prob_tuples = lda_trained_model.get_document_topics(doc_bow)
+        topic_id_prob_tuples = lda_model.get_document_topics(doc_bow)
 
         # iterate through the topic id/prob pairs
         for topic_id_prob in topic_id_prob_tuples:
@@ -132,11 +63,41 @@ def get_topic_ids_for_docs(lda_model, corpus):
             # store all topic probs for doc
             topic_probs.append(topic_prob)
 
-        # get the index for the topic that had the highest probability, for the current document
-        max_topic_prob_ind = np.argmax(topic_probs)
-        # get the corresponding topic id
-        max_prob_topic_id = topic_ids[max_topic_prob_ind]
-        # store the most probable topic id for the current document
-        doc_topic_ids.append(max_prob_topic_id)
+        # get index for topic with highest probability & associated topic id
+        likely_topic_id = topic_ids[np.argmax(topic_probs)]
+        doc_topic_ids.append(likely_topic_id)
 
     return doc_topic_ids
+
+def generate_word_cloud(topic_model, topic_id, topic_names, num_words=20):
+    '''
+    generates word cloud for topic with top (num_words) words most specific to select topic
+
+    parameters:
+        topic_model: Gensim object (must be trained model)
+        topic_id: int
+        topic_names: dictionary of topic names
+        num_words: int (default 20)
+    
+    returns:
+        no return (displays WordCloud plot)
+    '''
+
+    # get topic-term distribution
+    topic_terms = topic_model.get_topic_terms(topic_id, topn=topic_model.num_terms)
+    
+    # sort terms by topic-specific probability (descending)
+    sorted_terms = sorted(topic_terms, key=lambda x: x[1], reverse=True)
+    
+    # extract top words
+    words = [(topic_model.id2word[term_id], prob) for term_id, prob in sorted_terms[:num_words]]
+    
+    # Generate the word cloud
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(dict(words))
+    
+    # plot with presumed topic names
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.title(f"Exclusive Word Cloud for {topic_names[topic_id]}")
+    plt.show()
